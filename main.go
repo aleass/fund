8d606b7aeb7382442703d59a7acd7c6c7a229c01
guanziliang <gzljszgl@qq.com>
2024-02-26 09:32:18 +0800

    添加命令

--- main.go
+++ main.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fund/common"
 	"fund/service"
+	"os"
+	"strings"
 )
 
 func main() {
 	//初始化配置
 	common.InitConfig()
+	//命令启动
+	if len(os.Args) > 1 && os.Args[1] == "help" {
+		var command string
+		reader := bufio.NewReader(os.Stdin)
+		for {
+
+			println(`请输入:
+1.阶段收益
+2.净值
+3.已买基金
+q.退出
+`)
+			command, _ = reader.ReadString('\n')
+			//清楚无效字节
+			command = strings.ReplaceAll(command, "\r", "")
+			command = strings.ReplaceAll(command, "\n", "")
+			command = strings.TrimSpace(command)
+
+			//任务启动
+			switch command {
+			case "1": //阶段收益
+				println("执行:阶段收益")
+				service.Earnings.GetData()
+			case "2": //净值
+				println("执行:净值")
+				service.Data.GetData()
+			case "3": //已买基金
+				println("执行:已买基金")
+				service.Purchase.GetData()
+			case "q":
+				return
+			}
+			return
+		}
+	}
 
 	//任务启动
 	service.InitTask()
--- service/cron.go
+++ service/cron.go
@@ -4,41 +4,45 @@ import (
 	cron "github.com/robfig/cron/v3"
 )
 
+var (
+	List    = FundList{}
+	Star    = FundStar{}
+	BuySell = FundBuySell{}
+	//Task     = daysPastTimeRank{}
+	Purchase = FundPurchase{}
+	Data     = FundData{}
+	Earnings = FundEarnings{}
+	//EarningsRank = FundEaringsRank{}
+)
+
 // 定时
 func InitTask() {
-	list := FundList{}
-	star := FundStar{}
-	earnings := FundEarnings{}
-	//earningsRank := FundEaringsRank{}
-	buySell := FundBuySell{}
-	task := daysPastTimeRank{}
-	purchase := FundPurchase{}
-	data := FundData{}
+
 	//初始化更新
 	initFund := func() {
 		println("start")
-		list.GetData()
-		earnings.GetData()
-		data.GetData()
+		List.GetData()
+		Earnings.GetData()
+		Data.GetData()
 	}
 	initFund()
 	println("start task")
 	//触发定时
 	c := cron.New()
 	//阶段收益 1点
-	_, err := c.AddFunc("0 23 * * 2-6", earnings.GetData)
+	_, err := c.AddFunc("0 23 * * 2-6", Earnings.GetData)
 	if err != nil {
 		panic("cron err :" + err.Error())
 	}
 
 	//净值
-	_, err = c.AddFunc("15 23 * * 2-6", data.GetData)
+	_, err = c.AddFunc("15 23 * * 2-6", Data.GetData)
 	if err != nil {
 		panic("cron err :" + err.Error())
 	}
 
 	//已买基金
-	_, err = c.AddFunc("30 23 * * 2-6", purchase.GetData)
+	_, err = c.AddFunc("30 23 * * 2-6", Purchase.GetData)
 	if err != nil {
 		panic("cron err :" + err.Error())
 	}
@@ -50,31 +54,31 @@ func InitTask() {
 	}
 
 	//基金购买情况 0点
-	_, err = c.AddFunc("10 0 * * 2-6", buySell.GetData)
+	_, err = c.AddFunc("10 0 * * 2-6", BuySell.GetData)
 	if err != nil {
 		panic("cron err :" + err.Error())
 	}
 
-	//基金购买情况 2点
-	_, err = c.AddFunc("0 2 * * 2-6", task.Send)
-	if err != nil {
-		panic("cron err :" + err.Error())
-	}
+	////基金购买情况 2点
+	//_, err = c.AddFunc("0 2 * * 2-6", Task.Send)
+	//if err != nil {
+	//	panic("cron err :" + err.Error())
+	//}
 
 	//收益排行 0点
-	//_, err = c.AddFunc("0 0 * * 2-6", earningsRank.GetData)
+	//_, err = c.AddFunc("0 0 * * 2-6", EarningsRank.GetData)
 	//if err != nil {
 	//	panic("cron err :" + err.Error())
 	//}
 
 	//基金评级任务 每个月
-	_, err = c.AddFunc("0 4 * */1 *", star.GetData)
+	_, err = c.AddFunc("0 4 * */1 *", Star.GetData)
 	if err != nil {
 		panic("cron err :" + err.Error())
 	}
 
 	//基金列表 每个月
-	_, err = c.AddFunc("0 3 * */1 *", list.GetData)
+	_, err = c.AddFunc("0 3 * */1 *", List.GetData)
 	if err != nil {
 		panic("cron err :" + err.Error())
 	}
